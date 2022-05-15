653c4e81e800c86a23c5dbcd13b5ee8168abd774
fanylouise <gandra.fany@gmail.com>
2022-05-15 14:17:31 -0300

    modificando card do saiba mais

---
+++ assets/js/js/script.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  svg, 
+  .container-cabecalho img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
